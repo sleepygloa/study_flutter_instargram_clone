4da6ed8a48d7a69b1fd9a1209f92fae97212799e
sleepygloa <sleepygloa@gmail.com>
2021-04-08 08:40:29 +0900

    탭페이지 UI 및 상태 구현

--- lib/tag_page.dart
+++ lib/tag_page.dart
@@ -6,15 +6,32 @@ class TabPage extends StatefulWidget{
 }
 
 class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+  List _page = [
+    Text('page1'),
+    Text('page2'),
+    Text('page3'),
+  ];
+  
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items:<BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-        BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
-        BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Account')),
+      body : Center(child:_page[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        items:<BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Account')),
       ]),
     );
   }
 
+
+  void _onItemTapped(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
+  }
 }
