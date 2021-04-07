494d3fd48e20b4187b0b6eb30470e60877b3e3e0
sleepygloa <sleepygloa@gmail.com>
2021-04-07 17:48:52 +0900

    backup

---
+++ lib/root_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instargram_clone/tag_page.dart';
+
+class RootPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+  return TabPage();
+  }
+}
---
+++ lib/tag_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget{
+  @override
+  _TabPageState createState() => _TabPageState();
+  }
+}
+
+class _TabPageState extends State<TabPage> {
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      child:Text('탭 페이지');
+    );
+  }
+
+}
