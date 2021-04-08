1968d4f22c1908b2f2729867aab0b2546a33c3ee
sleepygloa <sleepygloa@gmail.com>
2021-04-08 15:18:00 +0900

    Search Page 구현

--- lib/tag_page.dart
+++ lib/tag_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instargram_clone/account_page.dart';
+import 'package:flutter_instargram_clone/search_page.dart';
 
 import 'home_page.dart';
 
@@ -12,7 +13,7 @@ class _TabPageState extends State<TabPage> {
   int _selectedIndex = 0;
   List _page = [
     HomePage(),
-    Text('page2'),
+    SearchPage(),
     AccountPage(),
   ];
 
